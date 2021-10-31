4b5427f987d339f7b1bd2b02a9da3749cd3e8536
shadialahmd <shadi.alahmd@gmail.com>
2021-10-31 11:58:09 +0400

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -2,6 +2,6 @@
         <div class="container" align="center">
 
     <h2 align="center" style="color:#fff"> Teacher Records Management System</h2>
-    <span style="color:#fff" >&copy;<a href="https://phpgurukul.com/" target="_blank"> PHPGurukul</a></span>
+    <span style="color:#fff" >&copy;<a href="#" target="_blank"> Shadi</a></span>
         </div>
     </footer>
